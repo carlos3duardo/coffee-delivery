70494cc8ba065df982d2f7a9c461613bf683c077
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-11-01 15:31:28 -0300

    Adicionando fontes do Google

--- src/styles/themes/global.ts
+++ src/styles/themes/global.ts
@@ -1,14 +1,24 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    body {
-        background-color: ${(props) => props.theme.background};
-        color: ${(props) => props.theme.baseText};
-    }
+  body {
+    background-color: ${(props) => props.theme.background};
+    color: ${(props) => props.theme.baseText};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Baloo 2', cursive;
+  }
 `

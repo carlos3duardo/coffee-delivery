532464fdd3a552bb90afa48704a6d1939de202a0
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-11-07 16:04:49 -0300

    Criando e estilizando o formulário de endereço

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,5 @@
 import { CurrencyDollar, MapPinLine } from "phosphor-react";
-import { CheckoutContainer, CardFormAddress, OrderInfo, CardPaymentMethod, ShoppingCart, Sidebar, CardTitle } from "./styles";
+import { CheckoutContainer, CardFormAddress, OrderInfo, CardPaymentMethod, ShoppingCart, Sidebar, CardTitle, FormGrid, GridItem, FormInput } from "./styles";
 
 export function Checkout() {
   return (
@@ -15,6 +15,16 @@ export function Checkout() {
               <span>Informe o endereço onde deseja receber seu pedido.</span>
             </div>
           </CardTitle>
+
+          <FormGrid>
+            <GridItem columnStart={1} columnEnd={5} requiredField><FormInput type="text" name="cep" id="cep" placeholder="CEP" required /></GridItem>
+            <GridItem columnStart={1} columnEnd={13} requiredField><FormInput type="text" name="rua" id="rua" placeholder="Rua" required /></GridItem>
+            <GridItem columnStart={1} columnEnd={5} requiredField><FormInput type="text" name="numero" id="numero" placeholder="Número" required /></GridItem>
+            <GridItem columnStart={5} columnEnd={13}><FormInput type="text" name="complemento" id="complemento" placeholder="Complemento" /></GridItem>
+            <GridItem columnStart={1} columnEnd={5} requiredField><FormInput type="text" name="bairro" id="bairro" placeholder="Bairro" required /></GridItem>
+            <GridItem columnStart={5} columnEnd={10} requiredField><FormInput type="text" name="cidade" id="cidade" placeholder="Cidade" required /></GridItem>
+            <GridItem columnStart={10} columnEnd={13} requiredField><FormInput type="text" name="uf" id="uf" placeholder="UF" required /></GridItem>
+          </FormGrid>
         </CardFormAddress>
 
         <CardPaymentMethod>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,11 +1,32 @@
 import styled from "styled-components";
 
-export const CheckoutContainer = styled.main`
+export const CheckoutContainer = styled.form`
   display: flex;
   gap: 2rem;
   width: 100%;
   max-width: 70rem;
   margin: 2rem auto 4rem auto;
+`
+
+export const CardTitle = styled.div<CardTitleProps>`
+  display: flex;
+  gap: 0.75rem;
+  align-items: flex-start;
+  margin-bottom: 2rem;
+
+  color: ${(props) => props.theme.baseSubtitle};
+
+  figure {
+    color: ${(props) => props.colorIcon === 'purple' ? props.theme.purple
+      : props.colorIcon === 'yellowDark' ? props.theme.yellowDark
+      : props.theme.baseSubtitle};
+  }
+
+  span {
+    display: block;
+    font-size: 0.875rem;
+    color: ${(props) => props.theme.baseText};
+  }
 `
 
 export const OrderInfo = styled.div`
@@ -24,13 +45,72 @@ export const Sidebar = styled.div`
   }
 `
 
-export const CardFormAddress = styled.form`
+export const CardFormAddress = styled.div`
   background-color: ${(props) => props.theme.baseCard};
 
   padding: 2.5rem;
   margin-bottom: 0.75rem;
 
   border-radius: 6px;
+`
+
+export const FormGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 0.75rem;
+  row-gap: 1rem;
+`
+
+interface GridItemProps {
+  columnStart?: number;
+  columnEnd?: number;
+  rowStart?: number;
+  rowEnd?: number;
+  requiredField?: boolean;
+}
+
+export const GridItem = styled.div<GridItemProps>`
+  grid-column-start: ${(props) => props.columnStart ?? 'auto'};
+  grid-column-end: ${(props) => props.columnEnd ?? 'auto'};
+
+  position: relative;
+
+  &::before {
+    color: ${(props) => props.theme.baseLabel};
+    display: ${(props) => !props.requiredField ? 'block' : 'none'};
+    position: absolute;
+    content: 'Opcional';
+    font-size: 0.75rem;
+    line-height: 0.75rem;
+    font-style: italic;
+    right: 0.75rem;
+
+    top: calc(50% - (0.75rem/2));
+  }
+`
+
+interface FormInputProps {
+  required?: boolean;
+}
+
+export const FormInput = styled.input<FormInputProps>`
+  color: ${(props) => props.theme.baseText};
+  background-color: ${(props) => props.theme.baseInput};
+
+  border: 1px solid ${(props) => props.theme.baseButton};
+  border-radius: 4px;
+
+  width: 100%;
+  padding: 0.75rem;
+
+  &:focus {
+    outline: none;
+    border-color: ${(props) => props.theme.yellowDark};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme.baseLabel}
+  }
 `
 
 export const CardPaymentMethod = styled.div`
@@ -44,28 +124,6 @@ export type ColorIcon = 'purple' | 'yellowDark';
 interface CardTitleProps {
   colorIcon?: ColorIcon;
 }
-
-
-
-export const CardTitle = styled.div<CardTitleProps>`
-  display: flex;
-  gap: 0.75rem;
-  align-items: flex-start;
-
-  color: ${(props) => props.theme.baseSubtitle};
-
-  figure {
-    color: ${(props) => props.colorIcon === 'purple' ? props.theme.purple
-      : props.colorIcon === 'yellowDark' ? props.theme.yellowDark
-      : props.theme.baseSubtitle};
-  }
-
-  span {
-    display: block;
-    font-size: 0.875rem;
-    color: ${(props) => props.theme.baseText};
-  }
-`
 
 export const ShoppingCart = styled.div`
   background-color: ${(props) => props.theme.baseCard};

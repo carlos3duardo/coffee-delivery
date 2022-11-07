6037dd0bb97ad904983e6bf942886d32b7374c71
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-11-07 09:12:51 -0300

    Adicionando links de navegação entre páginas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 import { Button, HeaderContainer, HeaderNavigation } from "./styles";
 
 export function Header() {
   return (
     <HeaderContainer>
-      <figure><img src="images/coffee-delivery-header-logo.png" /></figure>
+      <NavLink to="/" title="Home"><figure><img src="images/coffee-delivery-header-logo.png" /></figure></NavLink>
       <HeaderNavigation>
           <Button scheme="purpleLight"><MapPin size="1.125rem" weight="fill" /> João Pessoa, PB</Button>
-          <Button scheme="yellowLight"><ShoppingCart size="1.125rem" weight="fill" /></Button>
+          <NavLink to="/checkout" title="Carrinho de compras">
+            <Button scheme="yellowLight"><ShoppingCart size="1.125rem" weight="fill" /></Button>
+          </NavLink>
       </HeaderNavigation>
     </HeaderContainer>
   )

3a7398132708d17e064294b7c0615c6f4f2aec1f
Carlos Eduardo <carloseduardo@cenarioweb.com.br>
2022-11-07 09:09:12 -0300

    Criando um layout para reutilizar cabeçalho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import { HomeContainer } from './styles';
-import { Header } from '../../components/Header';
 import { Intro } from '../../components/Intro';
 import { Cardapio } from '../../components/Cardapio';
 
@@ -7,7 +6,6 @@ import { Cardapio } from '../../components/Cardapio';
 export function Home() {
   return (
     <HomeContainer>
-      <Header />
       <Intro />
       <Cardapio />
     </HomeContainer>
